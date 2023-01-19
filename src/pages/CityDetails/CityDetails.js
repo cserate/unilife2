9eabd054e57e7210f182c59f47deeb46eab420d7
Candice Serate <cwserate@gmail.com>
2023-01-18 22:20:14 -0500

    finished citydetails except for intext api data

--- src/pages/CityDetails/CityDetails.js
+++ src/pages/CityDetails/CityDetails.js
@@ -1,22 +1,38 @@
 import React from 'react'
 import Banner from '../../components/Banner/Banner'
-// import students from '../../../assets/person.png'
+import students from '../../assets/students.png'
 import './CityDetails.css'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import PropertyCard from '../../components/PropertyCard/PropertyCard'
 
-function CityDetails({property, length, address}) {
+function CityDetails({city, property}) {
     const baseUrl = "https://unilife-server.herokuapp.com";
     
     //need city id from url 
     const {id}= useParams()
-
     // console.log(id)
+
+    // //create state to hold cities
+    // const [cities, setCities] = React.useState([])
 
     //create state to hold properties
     const [properties, setProperties] = React.useState([])
-    // const [numProperties, setNumProperties] = React.useState([])
+
+
+// React.useEffect(
+//   ()=>{
+//       //call api to get cities
+//       axios.get(`${baseUrl}/cities`)
+//       // https://unilife-server.herokuapp.com/cities
+//       .then (res =>{
+//         console.log(res.data.response);
+//         //store the data from the api into state
+//         setCities(res.data.response)
+//       })
+//       .catch(err => console.log(err))
+// }, []
+// )
 
     //call api to direct to correct city
    React.useEffect(
@@ -39,25 +55,26 @@ function CityDetails({property, length, address}) {
     <div className='city-details-page'>
         <Banner />
         <p className='search-bar'>Search Bar</p>
+        <div className='city-property-count'>
+            {/* I can't figure this out! */}
+            {/* <h3>`${city.property_count} in ${city.name}`</h3>   */}
+        </div>
         <div className='property-cards-container'>
-            {/* <h3>{`${length} homes in ${property.address.city}`}</h3>
-             */}
-            {
+           {
             properties.map(item => <PropertyCard     
-                                          
+                                          key={item._id}
                                           property={item} 
-                                          image={item.images[0]} 
-                                          length={item.length}
-                                          address={item.address.city}/>)
+                                          image={item.images[0]} />)
             }
                               
         </div>
         <div className='about-city-container'>
             <div className='left-side-city'>
-                {/* <h3>Being a student in {property.address.city}</h3> */}
+               {/* I can't figure this out! */}
+                {/* <h3>`Being a student in ${property.address.city}</h3> */}
                 <p>?api data</p>
             </div>
-            {/* <img src='`${students} `'/> */}
+            <img className='students' src={students} />
             
          </div> 
     </div>
