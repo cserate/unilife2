5002ae81768896e49b68213c04f86ac021592ee5
Candice Serate <cwserate@gmail.com>
2023-01-20 20:46:04 -0500

    set up a part of the home details page

--- src/pages/HomeDetail/HomeDetail.js
+++ src/pages/HomeDetail/HomeDetail.js
@@ -2,14 +2,14 @@ import React from 'react'
 import './HomeDetail.css'
 import axios from 'axios';
 import { useParams } from 'react-router-dom'
-
+import {MdBed, MdOutlineBathtub} from 'react-icons/md'
 
 function HomeDetail() {
     const baseUrl = "https://unilife-server.herokuapp.com";
     
     //need home id from url 
     const {homeid}= useParams()
-    console.log(homeid)
+    // console.log(homeid)
 
     //create state to hold home data
     const [home, setHome] = React.useState([])
@@ -23,14 +23,44 @@ function HomeDetail() {
              .then (res =>{
                console.log(res.data);
                //store the data from the api into state
-              // setProperties(res.data.response)
+              setHome(res.data)
              })
              .catch(err => console.log(err))
        }, []
        )
   return (
     <div className='home-detail-container'>
-        HomeDetail
+        <div className='top-row'>
+            <div className='photo-grid'>
+              <img className='home' src={home.images} />
+            </div>
+            <div className='property-overview'>
+              <div className='address'>
+                <h2>{home.address?.street}, {home.address?.city}</h2>
+                <h2>{home.address?.postcode}</h2>
+              </div>
+              <div className='row1-titles'>
+                <small>Bedrooms</small>
+                <small>Bathrooms</small>
+                <small>Property Type</small>
+              </div>
+              <div className='row1-icons'>
+                  <div className='bedroom-info'>
+                    <MdBed />
+                    <small>{home.bedroom_count}</small>
+                  </div>
+                  <div className='bathroom-info'>
+                    <MdOutlineBathtub />
+                    <small>{home.bathroom_count}</small>
+                  </div>
+                  <div className='property-type-info'>
+                  <small>{home.property_type}</small>
+                  </div>
+              </div>
+              
+            </div>
+        </div>
+
     </div>
   )
 }
