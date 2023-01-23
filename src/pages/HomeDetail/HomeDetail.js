08ed40a0b7f99cd5990e261581884a62d77f4f21
Candice Serate <cwserate@gmail.com>
2023-01-22 22:29:20 -0500

    worked on top row right side

--- src/pages/HomeDetail/HomeDetail.js
+++ src/pages/HomeDetail/HomeDetail.js
@@ -3,6 +3,7 @@ import './HomeDetail.css'
 import axios from 'axios';
 import { useParams } from 'react-router-dom'
 import {MdBed, MdOutlineBathtub} from 'react-icons/md'
+import {BsCurrencyPound, BsHeart } from 'react-icons/bs'
 
 function HomeDetail() {
     const baseUrl = "https://unilife-server.herokuapp.com";
@@ -32,36 +33,68 @@ function HomeDetail() {
     <div className='home-detail-container'>
         <div className='top-row'>
             <div className='photo-grid'>
-              <img className='home' src={home.images} />
+              {/* <img className='home-big' src={home.images[0]} /> */}
+              <div className='home-3pic'>
+                {/* <img className='home2' src={home.images[1]} />
+                <img className='home3' src={home.images[2]} />
+                <img className='home4' src={home.images[3]} /> */}
+              </div>
             </div>
             <div className='property-overview'>
               <div className='address'>
-                <h2>{home.address?.street}, {home.address?.city}</h2>
-                <h2>{home.address?.postcode}</h2>
+                <h3>{home.address?.street}, {home.address?.city},</h3>
+                <h3>{home.address?.postcode}</h3>
               </div>
-              <div className='row1-titles'>
-                <small>Bedrooms</small>
-                <small>Bathrooms</small>
-                <small>Property Type</small>
-              </div>
-              <div className='row1-icons'>
-                  <div className='bedroom-info'>
-                    <MdBed />
-                    <small>{home.bedroom_count}</small>
+              <hr className='line-break'></hr>
+              <div className='row1'>
+                  <div className='bedrooms'>
+                    <small>Bedrooms</small>
+                    <div className='bedroom-info'>
+                      <MdBed className='bed-icon'/>
+                      <p>{home.bedroom_count}</p>
+                    </div>
                   </div>
-                  <div className='bathroom-info'>
-                    <MdOutlineBathtub />
-                    <small>{home.bathroom_count}</small>
+                  <div className='bathrooms'>
+                    <small>Bathrooms</small>
+                    <div className='bathroom-info'>
+                      <MdOutlineBathtub className='bath-icon'/>
+                      <p>{home.bathroom_count}</p>
+                    </div>
                   </div>
                   <div className='property-type-info'>
-                  <small>{home.property_type}</small>
+                    <small>Property Type</small>
+                    <p>{home.property_type}</p>
                   </div>
               </div>
-              
+              <div className='row2'>
+                  <div className='price'>
+                    <small>Price</small>
+                    <div className='price-info'>
+                      <BsCurrencyPound className='pound-icon'/>
+                      <p>{home.rent}</p>
+                    </div>
+                  </div>
+                  <div className='furnished-info'>
+                    <small>Furnished Type</small>
+                    <p>{home.furnished}</p>
+                  </div>
+                  <div className='available-info'>
+                    <small>Available</small>
+                    <p>{home.availability}</p>
+                  </div>
+              </div>
+              <div className='interest-container'>
+                <div className='shortlist'>
+                  <BsHeart className='heart-icon' />
+                  <p>Shortlist</p>
+                </div>
+                <button>Book Viewing</button>
+              </div>
             </div>
         </div>
-
     </div>
+
+    
   )
 }
 
