5537db2a7e19b8d9bdc6ea8a8f482bf5aec96a26
Candice Serate <cwserate@gmail.com>
2023-01-28 23:23:49 -0500

    finished photo grid and property overview, but still working on the rest

--- src/pages/HomeDetail/HomeDetail.js
+++ src/pages/HomeDetail/HomeDetail.js
@@ -33,64 +33,82 @@ function HomeDetail() {
     <div className='home-detail-container'>
         <div className='top-row'>
             <div className='photo-grid'>
-              {/* <img className='home-big' src={home.images[0]} /> */}
+              <img className='home-big' src={home.images[0]} />
               <div className='home-3pic'>
-                {/* <img className='home2' src={home.images[1]} />
+                <img className='home2' src={home.images[1]} />
                 <img className='home3' src={home.images[2]} />
-                <img className='home4' src={home.images[3]} /> */}
+                <img className='home4' src={home.images[3]} />
               </div>
             </div>
-            <div className='property-overview'>
-              <div className='address'>
-                <h3>{home.address?.street}, {home.address?.city},</h3>
-                <h3>{home.address?.postcode}</h3>
-              </div>
-              <hr className='line-break'></hr>
-              <div className='row1'>
-                  <div className='bedrooms'>
-                    <small>Bedrooms</small>
-                    <div className='bedroom-info'>
-                      <MdBed className='bed-icon'/>
-                      <p>{home.bedroom_count}</p>
+            <div className='top-right'>
+              <div className='property-overview'>
+                <div className='address'>
+                  <h3>{home.address?.street}, {home.address?.city},</h3>
+                  <h3>{home.address?.postcode}</h3>
+                </div>
+                <hr className='line-break'></hr>
+                <div className='row1'>
+                    <div className='bedrooms'>
+                      <small>Bedrooms</small>
+                      <div className='bedroom-info'>
+                        <MdBed className='bed-icon'/>
+                        <p>{home.bedroom_count}</p>
+                      </div>
                     </div>
-                  </div>
-                  <div className='bathrooms'>
-                    <small>Bathrooms</small>
-                    <div className='bathroom-info'>
-                      <MdOutlineBathtub className='bath-icon'/>
-                      <p>{home.bathroom_count}</p>
+                    <div className='bathrooms'>
+                      <small>Bathrooms</small>
+                      <div className='bathroom-info'>
+                        <MdOutlineBathtub className='bath-icon'/>
+                        <p>{home.bathroom_count}</p>
+                      </div>
                     </div>
-                  </div>
-                  <div className='property-type-info'>
-                    <small>Property Type</small>
-                    <p>{home.property_type}</p>
-                  </div>
-              </div>
-              <div className='row2'>
-                  <div className='price'>
-                    <small>Price</small>
-                    <div className='price-info'>
-                      <BsCurrencyPound className='pound-icon'/>
-                      <p>{home.rent}</p>
+                    <div className='property-type-info'>
+                      <small>Property Type</small>
+                      <p>{home.property_type}</p>
                     </div>
-                  </div>
-                  <div className='furnished-info'>
-                    <small>Furnished Type</small>
-                    <p>{home.furnished}</p>
-                  </div>
-                  <div className='available-info'>
-                    <small>Available</small>
-                    <p>{home.availability}</p>
-                  </div>
+                </div>
+                <div className='row2'>
+                    <div className='price'>
+                      <small>Price</small>
+                      <div className='price-info'>
+                        <BsCurrencyPound className='pound-icon'/>
+                        <p>{home.rent}</p>
+                      </div>
+                    </div>
+                    <div className='furnished-info'>
+                      <small>Furnished Type</small>
+                      <p>{home.furnished}</p>
+                    </div>
+                    <div className='available-info'>
+                      <small>Available</small>
+                      <p>{home.availability}</p>
+                    </div>
+                </div>
               </div>
               <div className='interest-container'>
-                <div className='shortlist'>
-                  <BsHeart className='heart-icon' />
-                  <p>Shortlist</p>
-                </div>
-                <button>Book Viewing</button>
+                  <div className='shortlist'>
+                    <BsHeart className='heart-icon' />
+                    <p>Shortlist</p>
+                  </div>
+                  <button>Book Viewing</button>
               </div>
             </div>
+        </div>
+        <div className='middle-row'>
+          <div className='description-container'>
+            <h3>Description</h3>
+            <p>{home.property_description}</p>
+          </div>
+          <div className='bedroom-prices'>
+            <h3>Bedroom Prices</h3>
+            <div className='bedroom1'>
+              <p>Bedroom 1</p>
+              <div className='bedroom1-rent'>
+                <BsCurrencyPound className='pound-icon'/>
+                <p>{home.bedroom_prices.bedroom_one} per week</p>
+              </div>
+            </div>
+          </div>
         </div>
     </div>
 
